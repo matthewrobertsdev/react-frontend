30cb23abaed6fa2163a583c44f1a7d16b9540e17
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-11-25 23:44:21 -0500

    Some fixes for basic set-up

--- src/Actions.js
+++ src/Actions.js
@@ -1,11 +1,16 @@
 import homeConnect from './HomeConnect';
 
+const _getPosts = () => ({
+    type: 'GET_POSTS',
+    posts
+});
+
 //gets all topics for now for teaching
-export const getTopics = () => {
+export const getPosts = () => {
     return (dispatch) => {
         return homeConnect.get('posts').then(response => {
             console.log(response.data);
-            dispatch(_getTopics(response.data));
+            //dispatch(_getTopics(response.data));
         }).catch(function(error) {
             console.log(error.response.data);
          });
--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './Store';
 import './App.css';
+import homeConnect from './HomeConnect';
 
-function App() { return (<Provider store={store}> {console.log('started')} </Provider>); }
+function App() { return (<Provider store={store}> {console.log('started')} 
+
+{helloWorld()}
+
+</Provider>); }
 
 export default App;
+
+function helloWorld(){
+    {homeConnect.get('posts').then(response => {
+        console.log(response.data);
+        //dispatch(_getTopics(response.data));
+    }).catch(function(error) {
+        //console.log(error.response.data);
+     })}
+}
